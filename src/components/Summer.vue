3a804b9f19808a914d13fbbc4ba793c54b3802fc
Tochi <ukwuomatochi112@gmail.com>
2023-06-21 23:43:01 +0100

    added the summer section

---
+++ src/components/Summer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="summer_section">
+    <div class="summer_img_div">
+      <img src="@/assets/summer-trip1.jpg" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Summer",
+};
+</script>
+
+<style scoped>
+.summer_section {
+  padding: 3rem 0;
+}
+.summer_img_div {
+  width: max-content;
+  height: max-content;
+  overflow-x: hidden;
+}
+.summer_img_div img {
+  width: 100vw;
+  /* height: 38rem; */
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <Landing />
     <Favorite />
+    <Summer />
   </div>
 </template>
 
@@ -9,11 +10,13 @@
 // @ is an alias to /src
 import Landing from "@/components/Landing.vue";
 import Favorite from "@/components/Favorite.vue";
+import Summer from "@/components/Summer.vue";
 export default {
   name: "HomeView",
   components: {
     Landing,
     Favorite,
+    Summer,
   },
 };
 </script>
