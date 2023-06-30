cc52fe3da2e02f46a2c5db25ffe69defde9df308
Tochi <ukwuomatochi112@gmail.com>
2023-06-30 14:15:50 +0100

    almost done with the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -65,9 +65,48 @@
           <p>California Transparency Act</p>
           <p>Community Offers</p>
         </div>
+        <!-- <div></div>
         <div></div>
-        <div></div>
-        <div></div>
+        <div></div> -->
+      </div>
+
+      <div class="second-section">
+        <div class="follow">
+          <h4>FOLLOW THE FLOCK</h4>
+          <P>
+            Exclusive offers, a heads up on new things, and sightings of
+            Allbirds in the wild. Oh, we have cute sheep, too. #weareallbirds
+          </P>
+          <div class="socials-div">
+            <img src="@/assets/insta.svg" alt="instagram" />
+            <img src="@/assets/twitter2.svg" alt="twitter" />
+            <img src="@/assets/fb.svg" alt="facebook" />
+            <img src="@/assets/pinterest.svg" alt="pinterest" />
+          </div>
+        </div>
+
+        <div class="certified">
+          <img src="@/assets/icon-b-corp.svg" alt="icons" />
+        </div>
+
+        <div class="flag-div">
+          <img src="@/assets/flag1.svg" alt="flag" />
+          <img src="@/assets/flag2.svg" alt="flag" />
+          <img src="@/assets/flag3.svg" alt="flag" />
+          <img src="@/assets/flag4.svg" alt="flag" />
+          <img src="@/assets/flag5.svg" alt="flag" />
+          <img src="@/assets/flag6.svg" alt="flag" />
+          <img src="@/assets/flag7.svg" alt="flag" />
+          <img src="@/assets/flag8.svg" alt="flag" />
+        </div>
+      </div>
+
+      <div class="copyright">
+        <p>
+          &copy; 2022 Allbirds, Inc. All Rights Reserved. Terms, Privacy &
+          Accessibility
+        </p>
+        <p><a href="#">Do Not Sell My Personal Information</a></p>
       </div>
     </div>
   </div>
@@ -117,6 +156,16 @@ export default {
 .footer-header .policy {
   font-size: small;
 }
+.copyright {
+  text-align: center;
+  font-size: small;
+}
+.copyright p {
+  margin-bottom: 0.5rem;
+}
+.copyright a {
+  color: #fff;
+}
 .footer-header .policy a {
   color: #4d5559;
   font-weight: 900;
@@ -139,30 +188,48 @@ button {
   background-color: #212a2f;
   display: none;
 }
-
 .footer-footer {
   background-color: #212a2f;
   padding: 4rem 2rem;
   color: #fff;
 }
 .first-section {
-  display: flex;
-  justify-content: space-between;
-  /* align-items: center; */
+  display: grid;
+  /* justify-content: space-between; */
+  grid-template-columns: repeat(4, 1fr);
 }
-.first-section p {
+.second-section {
+  display: grid;
+  grid-template-columns: 1fr 1fr 2.8fr;
+  gap: 6rem;
+  margin: 4rem 0;
+}
+.flag-div img {
+  margin-right: 1.5rem;
+}
+.first-section p,
+.second-section p {
   font-size: small;
   margin: 0.5rem 0;
 }
+.socials-div {
+  display: flex;
+  gap: 1.5rem;
+}
 
-@media screen and (max-width: 960px) {
-  .first-section {
+@media screen and (max-width: 1024px) {
+  .first-section,
+  .second-section {
     display: flex;
     justify-content: center;
     flex-direction: column;
     text-align: center;
     gap: 3rem;
-    /* align-items: center; */
+    margin: 2.2rem 0;
+  }
+  .socials-div {
+    justify-content: center;
+    margin-top: 1rem;
   }
 }
 @media screen and (max-width: 720px) {
@@ -179,7 +246,6 @@ button {
   button {
     display: block;
     width: 75vw;
-    /* width: 100vw; */
   }
   .footer-header .form input {
     width: 75vw;
